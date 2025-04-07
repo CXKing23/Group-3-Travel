143d1c42bc75a236fc482804694677c4df2d1d44
Hafsah Kashmiri <134805782+Hkashmiri@users.noreply.github.com>
2025-04-07 13:23:11 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,50 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/Header"
+import Home from "./pages/Home";
+import Planner from "./pages/Planner";
+import DestinationPicker from "./pages/DestinationPicker.jsx";
+import PackingChecker from "./pages/PackingChecker";
+import Itinerary from "./pages/Itinerary";
+import TravelAdvice from "./pages/TravelAdvice";
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import Footer from "./components/Footer.jsx";
+import Countries from  "./pages/countries.jsx";
+import CountryRouter from "./pages/CountryRouter.jsx";
+import Norway from "./pages/countries/norway.jsx";
+import Submit from "./pages/Submit"; // adjust the path as needed
+import BrowseStories from './pages/BrowseStories';
+
+
+
+function App() {
+  return (
+    
+    <Router>
+      <Header></Header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/planner" element={<Planner />} />
+        <Route path="/destination-picker" element={<DestinationPicker />} />
+        <Route path="/packing-checker" element={<PackingChecker />} />
+        <Route path="/itinerary" element={<Itinerary />} />
+        <Route path="/travel-advice" element={<TravelAdvice />} />
+        <Route path="/about-us" element={<AboutUs />} />
+        <Route path="/contact-us" element={<ContactUs />} />
+        <Route path="/submit" element={<Submit />} />
+        <Route path="/browse-stories" element={<BrowseStories />} />
+
+
+        
+        <Route path="/countries" element={<Countries />} />
+        <Route path="/countries/norway" element={<Norway />} />
+      </Routes>
+      <CountryRouter />
+      <Footer></Footer>
+    </Router>
+
+  )
+}
+
+export default App;
